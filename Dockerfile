1c59d6c3160a2877066eeb5a8f55743899267a74
Álvaro González <Alvaro.Gonzalez@csc.fi>
2022-09-09 09:51:51 +0300

    Minor modifications to allow changing the parameters passed to the oom-killer

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
 FROM python:3-alpine
 RUN mkdir /app
 ADD app.py /app/
-CMD ["sh","-c","python3 /app/app.py"]
+ENTRYPOINT ["/app/app.py"]
--- app.py
+++ app.py
@@ -1,17 +1,40 @@
 #!/usr/bin/env python3
-
-# Reserve memory until OOM killer hits
+'''
+Reserves N memory chunks of 10MB each. Waiting 2 seconds in between each reserve.
+'''
 
 import time
+import sys
 
-def main(N=100):
-  a = []
-  for i in range(0,N):
-    a.append(bytearray(10000000)) # add 10MB to array
-    print("{0}0MB".format(i+1), flush=True)
-    time.sleep(2)
+def main(number_chunks, chunk_size, sleep_time):
+    '''
+    Main function, number_chunks is the number 10MB chunks to reserve
+    '''
+    array = []
+    for i in range(0,number_chunks):
+        array.append(bytearray(chunk_size*1000000)) # add 10MB to array
+        print(f"{(i+1)*chunk_size}MB", flush=True)
+        time.sleep(sleep_time)
 
+    print("Sleeping...")
+    # Infinite loop, to keep it alive
+    while True:
+        time.sleep(5)
 
 if __name__ == "__main__":
-  main()
+    try:
+        NUMBER=int(sys.argv[1])
+    except IndexError as ierr:
+        NUMBER=100
 
+    try:
+        CHUNK_SIZE=int(sys.argv[2])
+    except IndexError as ierr:
+        CHUNK_SIZE=10
+
+    try:
+        SLEEP_TIME=int(sys.argv[3])
+    except IndexError as ierr:
+        SLEEP_TIME=2
+
+    main(number_chunks=NUMBER,chunk_size=CHUNK_SIZE,sleep_time=SLEEP_TIME)
